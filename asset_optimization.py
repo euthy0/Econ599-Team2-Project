1b30f6e6ab2d72cdfea22797b5dfdfdfc33ad3aa
Cody <90341326+euthy0@users.noreply.github.com>
2022-03-11 15:31:54 -0700

    Add files via upload

--- asset_optimization.py
+++ asset_optimization.py
@@ -6,25 +6,36 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 
 
-def bootstrapping(data, visual):
+def bootstrapping(data):
     my_samples = []
+    negatives = 0
+
     for i in range(5000):
-        x = np.random.choice(data, size=12, replace=True)
+        x = np.random.choice(data[:-1], size=12, replace=True)
+
         my_samples.append(x.mean())
 
-    if visual is True:
-        plt.hist(my_samples)
+        if x.mean() < 0:
+            negatives += 1
 
-    return statistics.median(my_samples)
+    proportion_neg = negatives/5000
+
+    return statistics.mean(my_samples), proportion_neg
 
 
 def asset_return(assets, data):
     flat_rate = 1/len(assets)
+    individual_return = []
+    negative_prop = []
     package_return = 0
-    for i in assets:
-        package_return += flat_rate*bootstrapping(data[i])
 
-    return package_return
+    for i in assets:
+        bootstrap_result = bootstrapping(data[i])
+        package_return += flat_rate*bootstrap_result[0]
+        individual_return.append(bootstrap_result[0])
+        negative_prop.append(bootstrap_result[1])
+
+    return package_return, individual_return, negative_prop
 
 
 def correlation_matrix(data):
@@ -36,7 +47,7 @@ def correlation_matrix(data):
 
 def main():
     data = pandas.read_csv('data_w_cpi.csv')
-
+    row_data = []
     assets = ['US LT (20Y) Corporate Bonds (TR)', 'US LT (20Y) Government Bonds (TR)',
               'US Intermediate-term (5Y) Government Bonds (TR)', 'US (30-Day) Treasury Bills',
               'AUSTRALIA Standard (Large+Mid Cap)', 'EUROPE Standard (Large+Mid Cap)',
@@ -45,9 +56,26 @@ def main():
               'Gold Return', 'CANADA Standard (Large+Mid Cap)', 'US Large-Cap Stocks (TR)',
               'US Small-Cap Stocks (TR)']
 
-    correlation_matrix(data)
-    #results = asset_return(assets, data)
-    #print(results)
+    #correlation_matrix(data)
+
+    results = asset_return(assets, data)
+
+    print(results[0])
+
+    n = 0
+
+    with open("negative_prop.csv", 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        headers = ["Asset", "Asset Return", "Negative Proportion"]
+        writer.writerow(headers)
+        for i in assets:
+            row_data = []
+            row_data.append(i)
+            row_data.append(results[1][n])
+            row_data.append(results[2][n])
+            writer.writerow(row_data)
+
+            n += 1
     #bootstrapping(data[assets[1]], True)
 
 
